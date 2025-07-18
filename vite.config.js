629036704a0c9ea0b4926d8b9b3755be76bdcc37
Kashyap Tatmiya <kashyap.it@gmail.com>
2025-07-18 14:00:08 +0530

    moving to neo mophism, added profile pic

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@assets': path.resolve(__dirname, './assets')
+    }
+  },
   base: './',
   css: {
     postcss: {
